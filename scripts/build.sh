81b5567f009247a1401efc44126988f0e0f162db
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-03-06 17:12:57 +0000

    build.sh: Fix strip file
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,7 +18,7 @@ function buildProject()
 if [[ "$MODE" == "release" ]]; then
     COMPILER="ldc2"
     buildProject
-    strip ./serpent
+    strip ./serpent-demo-rpg
 elif [[ "$MODE" == "optimized" ]]; then
     MODE="release"
     buildProject
